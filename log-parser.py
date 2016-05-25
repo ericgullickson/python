5edd9c9d72d68b3e6f0022076e5675fd108ba741
Eric Gullickson <egullickson@capital-data.com>
2016-05-25 12:17:30 -0500

    Cleaned up some formatting

--- log-parser.py
+++ log-parser.py
@@ -16,55 +16,58 @@ def main():
 	                )
 	#This is the type of log file we want to parse. 
 	objParser.add_argument(
-					"-t", 
-					"--type", 
-					dest="strType",
-	                help="The type of logfile to parse. Available options are: cisco-ios,cisco-nxos,junos", 
-	                metavar="TYPE",
-	                nargs='?',
-	                default="check_string_for_empty"
-	                )
+		"-t", 
+		"--type", 
+		dest="strLogType",
+		help="The type of logfile to parse. Available options are: cisco-ios,cisco-nxos,junos", 
+		metavar="TYPE",
+		nargs='?',
+		default="check_string_for_empty"
+		)
 
 	#Parse all our arguments into an object to be used later
 	objArguments = objParser.parse_args()
 	#After we have arguments built into a different argument object. We pass that to our sanitation functions. 
-	boolTypeError = error_check_type_arg(objArguments.strType)
 	boolFileError = error_check_file_exists(objArguments.strFileName)
+	boolTypeError = error_check_type_arg(objArguments.strLogType)
 	if (boolTypeError == True) or (boolFileError == True):
 		objParser.print_help()
 
-def process_cisco_ios_logs():
-	print 'Processing Cisco IOS Logs'
+		def process_cisco_ios_logs():
+			print 'Processing Cisco IOS Logs'
 
-def process_cisco_nxos_logs():
-	print 'Processing Cisco NX-OS Logs'
+			def process_cisco_nxos_logs():
+				print 'Processing Cisco NX-OS Logs'
 
-def process_junos_logs():
-	print 'Processing Juniper JunOS Logs'
+				def process_junos_logs():
+					print 'Processing Juniper JunOS Logs'
 
-def error_check_type_arg(strType):
-	if strType in dicType.keys():
-		dicType[strType]()
-	else:
-		print 'ERROR: You specified an invalid file type'
-		return True
+					def error_check_type_arg(strLogType):
+						if strLogType in dicLogType.keys():
+							dicLogType[strLogType]()
+						else:
+							print 'ERROR: You specified an invalid file type'
+							return True
 
-def error_check_file_exists(strFileName):
-	if os.path.isfile(strFileName):
-		objLogFile = open(strFileName)
-	else:
-		print 'ERROR: You specified an invalid file. Be sure to give full path.'
-		return True
+							def error_check_file_exists(strFileName):
+								if os.path.isfile(strFileName):
+									objLogFile = open(strFileName)
+									return objLogFile
+								else:
+									print 'ERROR: You specified an invalid file. Be sure to give full path.'
+									return True
 
+#If the user didn't input any of the required arguments let them know. I didn't feel like passing an parser object just to print the pretty help.
 def empty_argument_warning():
 	print 'ERROR: You didn\'t specify a type of file or file that exists. Use -h for help.'
 
 #This is the dictionary of possible file types and associated functions. It also includes the default value we set in the argument definition for empty file type. 
-dicType = {
-		"check_string_for_empty": empty_argument_warning,
-		"cisco-ios": process_cisco_ios_logs,
-		"cisco-nxos": process_cisco_nxos_logs,
-		"junos": process_junos_logs,
-	}
+dicLogType = {
+"check_string_for_empty": empty_argument_warning,
+"cisco-ios": process_cisco_ios_logs,
+"cisco-nxos": process_cisco_nxos_logs,
+"junos": process_junos_logs,
+}
 
+#Let's get this party started!
 main()
